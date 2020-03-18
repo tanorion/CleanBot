3f88210941a881685882bb5a75fb0cc32ae5a2cb
Niklas Wanngren <Niklas.Wanngren@adlibris.com>
2020-03-18 08:37:42 +0100

    init commit

---
+++ CleanBot/Abstractions/ICleaningService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Abstractions
+{
+    public interface ICleaningService
+    {
+        void Clean();
+    }
+}
---
+++ CleanBot/CleanBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CleanBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
+
+            //configure console logging
+            serviceProvider
+                .GetService<ILoggerFactory>();
+             
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+            logger.LogDebug("Starting application");
+
+            //do the actual work here
+            var bar = serviceProvider.GetService<ICleaningService>();
+            bar.Clean();
+
+            logger.LogDebug("All is clean");
+
+        }
+    }
+}
